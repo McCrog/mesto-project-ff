115691f47ac98867f7197c70eca8f32041fc1fad
McCrog <pit_mib@bk.ru>
2023-11-23 16:25:06 +0300

    show popups

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ import cardOneImage from './images/card_1.jpg';
 import cardTwoImage from './images/card_2.jpg';
 import cardThreeImage from './images/card_3.jpg';
 
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
+
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
+
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const POPUP_IS_OPENED_CLASS = 'popup_is-opened';
 
 function createCard(cardItem, deleteCardCallback) {
   const card = cardTemplate.cloneNode(true);
@@ -31,3 +39,39 @@ function showCards() {
 }
 
 showCards();
+
+profileEditButton.addEventListener('click', () => {
+  showPopup(popupTypeEdit);
+});
+
+profileAddButton.addEventListener('click', () => {
+  showPopup(popupTypeNewCard);
+});
+
+function showPopup(popupElement) {
+  popupElement.classList.add(POPUP_IS_OPENED_CLASS);
+
+  const popupClose = popupElement.querySelector('.popup__close');
+  popupClose.addEventListener('click', handlePopupCloseClick);
+  document.addEventListener('click', handleOverlayClick);
+  document.addEventListener('keydown', handleEscapeKeyDown);
+
+  function handlePopupCloseClick() {
+    popupClose.removeEventListener('click', handlePopupCloseClick);
+    document.removeEventListener('click', handleOverlayClick);
+    document.removeEventListener('keydown', handleEscapeKeyDown);
+    popupElement.classList.remove(POPUP_IS_OPENED_CLASS);
+  }
+
+  function handleEscapeKeyDown(evt) {
+    if (evt.key === 'Escape') {
+      handlePopupCloseClick();
+    }
+  }
+
+  function handleOverlayClick(evt) {
+    if (evt.target.classList.contains(POPUP_IS_OPENED_CLASS)) {
+      handlePopupCloseClick();
+    }
+  }
+}
